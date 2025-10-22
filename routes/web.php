4e4a10a2e6d739f8ef3644bdad50b534a8dec543
erika2SIE <erika24si@mahasiswa.pcr.ac.id>
2025-10-22 10:30:07 +0700

    tugas7

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\PelangganController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -32,4 +33,7 @@ Route::get('/home', [HomeController::class, 'index'])->name ('home');
 Route::post('question/store', [QuestionController::class, 'store'])
 		->name('question.store');
 
+
 Route::resource('pelanggan', PelangganController::class);
+
+Route::resource ('user', UserController::class);
